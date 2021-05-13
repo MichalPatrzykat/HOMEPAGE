64d91768118e40c45999239263ef9ca39a2e0b7a
Michał Patrzykąt <patrzykat13@gmail.com>
2021-05-13 16:53:39 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+console.log("Pozdrawiam Serdecznie!")
+
+
+
+let button = document.querySelector(".button");
+let  header_tittle= document.querySelector(".header_tittle");
+
+button.addEventListener("click", () => {
+    header_tittle.classList.toggle("hideme");
+    button.innerText =
+    header_tittle.classList.contains("hideme") ? "Skasuj Nagłówek" : "Pokaż nagłówek";
+    
+
+});
+let button_contactButtonNumber = document.querySelector(".button_contactButtonNumber");
+let phoneNumber = document.querySelector(".phoneNumber");
+button_contactButtonNumber.addEventListener("click", () => {
+    phoneNumber.classList.toggle("hidenumber");
+    button_contactButtonNumber.innerText =
+    phoneNumber.classList.contains("hidenumber") ? "Pokaż kontakt" : "Ukryj kontakt";
+});
+
+
+console.log(button_contactButtonNumber);
+
+
